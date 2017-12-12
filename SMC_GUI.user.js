9d84d86cc79ad1cdb9affa2ffab7e4294cfd6b27
Guido Erlinger <Guido.Erlinger@br-automation.com>
2017-12-12 16:53:49 +0100

    Version 2.20: just changed the version number

--- SMC_GUI.user.js
+++ SMC_GUI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        GUI
 // @namespace   SMCTools
-// @version     2.13
+// @version     2.20
 // @description Displays information about products in SMC product repository
 // @grant       none
 // @include     http://smc*
--- SMC_ProductInfo.user.js
+++ SMC_ProductInfo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        ProductInfo
 // @namespace   SMCTools
-// @version     2.12
+// @version     2.20
 // @description Displays information about products in SMC product repository
 // @grant       none
 // @include     http://smc*
