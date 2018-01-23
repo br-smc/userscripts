798a15e26c7856ef190c8866d7bf064acda339c1
BR-SMC <33719896+br-smc@users.noreply.github.com>
2018-01-23 13:32:54 +0100

    Added @updateURL

--- SMC_GUI.user.js
+++ SMC_GUI.user.js
@@ -8,6 +8,7 @@
 // @require     http://code.jquery.com/jquery-3.2.1.min.js
 // @run-at      document-idle
 // @downloadURL https://github.com/br-smc/userscripts/raw/master/SMC_GUI.user.js
+// @updateURL https://github.com/br-smc/userscripts/raw/master/SMC_GUI.user.js
 // ==/UserScript==
 
 var GUI = window.GUI || {};
