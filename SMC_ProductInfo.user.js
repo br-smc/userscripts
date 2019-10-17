bd04a0641ddc4e13cc2e82d967574a208a905aea
BR-SMC <33719896+br-smc@users.noreply.github.com>
2019-10-17 18:00:17 +0200

    Version 2.62
    Changed @match urls

--- SMC_ProductInfo.user.js
+++ SMC_ProductInfo.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        ProductInfo
 // @namespace   https://github.com/br-smc/userscripts
-// @version     2.61
+// @version     2.62
 // @description Displays information about products in SMC product repository
 // @author      Guido Erlinger
 // @grant       none
-// @match       http://*/SMC3/plugin-transformer
-// @match       http://*/plugin-transformer
+// @match       http://smc.br-automation.com/SMC3/plugin-transformer
+// @match       http://smc-qs.br-automation.com/plugin-transformer
 // @require     https://code.jquery.com/jquery-3.4.1.min.js
 // @downloadURL https://github.com/br-smc/userscripts/raw/master/SMC_ProductInfo.user.js
 // @updateURL   https://github.com/br-smc/userscripts/raw/master/SMC_ProductInfo.meta.js
