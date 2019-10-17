d1beb804ce51eb817992250222c468c1a7bd9bf0
BR-SMC <33719896+br-smc@users.noreply.github.com>
2019-10-17 17:55:14 +0200

    Version 2.40

--- SMC_GUI.meta.js
+++ SMC_GUI.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        GUI
 // @namespace   https://github.com/br-smc/userscripts
-// @version     2.30
+// @version     2.40
 // @description Change some SMC GUI elements
 // @author      Guido Erlinger
 // @grant       none
-// @include     http://smc*
-// @require     https://code.jquery.com/jquery-3.3.1.min.js
+// @match       http://smc.br-automation.com/SMC3/
+// @match       http://smc-qs.br-automation.com/
 // @downloadURL https://github.com/br-smc/userscripts/raw/master/SMC_GUI.user.js
 // @updateURL   https://github.com/br-smc/userscripts/raw/master/SMC_GUI.meta.js
 // @run-at      document-idle
