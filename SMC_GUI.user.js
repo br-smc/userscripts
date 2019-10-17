f4aeb1a5ec9fccc688c8fb9c9da195bc4184fc3c
BR-SMC <33719896+br-smc@users.noreply.github.com>
2019-10-17 17:54:47 +0200

    Version 2.40
    Removed jquery resource

--- SMC_GUI.user.js
+++ SMC_GUI.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name        GUI
 // @namespace   https://github.com/br-smc/userscripts
-// @version     2.30
+// @version     2.40
 // @description Change some SMC GUI elements
 // @author      Guido Erlinger
 // @grant       none
-// @include     http://smc*
-// @require     https://code.jquery.com/jquery-3.3.1.min.js
+// @match       http://smc.br-automation.com/SMC3/
+// @match       http://smc-qs.br-automation.com/
 // @downloadURL https://github.com/br-smc/userscripts/raw/master/SMC_GUI.user.js
 // @updateURL   https://github.com/br-smc/userscripts/raw/master/SMC_GUI.meta.js
 // @run-at      document-idle
 // ==/UserScript==
 
-var GUI = window.GUI || {};
+console.log("TEST");
+
+var GUI = top.GUI || {};
 
 GUI.MAIN = (function(){
   return {
@@ -67,10 +69,7 @@ GUI.ExplorerPaginator = (function(){
   };
 })();
 
-
 $(document).ready(function(){
-  setTimeout(function(){
-    GUI.MAIN.Format();
-    GUI.ExplorerPaginator.Format();
-  }, 1000);
+  GUI.MAIN.Format();
+  GUI.ExplorerPaginator.Format();
 });
