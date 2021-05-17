91fafb6cd7727d9b1487f767d49e1f346d74fa29
Guido Erlinger <guido.erlinger@br-automation.com>
2021-05-17 18:55:17 +0200

    Adapt scripts for SMC 4.3.

--- SMC_GUI.user.js
+++ SMC_GUI.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        GUI
 // @namespace   https://github.com/br-smc/userscripts
-// @version     2.40
+// @version     3.00
 // @description Change some SMC GUI elements
 // @author      Guido Erlinger
 // @grant       none
-// @match       http://smc.br-automation.com/SMC3/
-// @match       http://smc-qs.br-automation.com/
+// @match       http://smc.br-automation.com/
+// @match       http://smc-qs.br-automation.co.at/
 // @downloadURL https://github.com/br-smc/userscripts/raw/master/SMC_GUI.user.js
 // @updateURL   https://github.com/br-smc/userscripts/raw/master/SMC_GUI.meta.js
 // @run-at      document-idle
--- SMC_ProductInfo.meta.js
+++ SMC_ProductInfo.meta.js
@@ -5,9 +5,9 @@
 // @description Displays information about products in SMC product repository
 // @author      Guido Erlinger
 // @grant       none
-// @match       http://smc.br-automation.com/SMC3/plugin-transformer?*
-// @match       http://smc-qs.br-automation.com/plugin-transformer?*
-// @require     https://code.jquery.com/jquery-3.4.1.min.js
+// @match       http://smc.br-automation.com/plugin-transformer?*
+// @match       http://smc-qs.br-automation.co.at/plugin-transformer?*
+// @require     https://code.jquery.com/jquery-3.6.0.min.js
 // @downloadURL https://github.com/br-smc/userscripts/raw/master/SMC_ProductInfo.user.js
 // @updateURL   https://github.com/br-smc/userscripts/raw/master/SMC_ProductInfo.meta.js
 // @run-at      document-idle
